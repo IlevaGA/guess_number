7265ef5f8721696eaedbaf77fa390cae5416e2a7
Галина Ильева <induffer@gmail.com>
2024-02-03 13:20:16 +0700

    add initial commit

---
+++ guess_number.py
@@ -0,0 +1,16 @@
+import random
+
+random_num = random.randint(1, 100)
+print("Угадай число от 1 до 100")
+
+while True:
+    person_num = int(input())
+    if person_num < random_num:
+        print("Ваше число меньше того, что загадано")
+        continue
+    elif person_num > random_num:
+        print("Ваше число больше того, что загадано")
+        continue
+    else:
+        print("Отличная интуиция! Вы угадали число :)")
+        break
